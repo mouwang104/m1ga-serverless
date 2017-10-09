46e2645a9400385cad64a50c85314ecfdd4426b3
MouDev-HACK <moudev-hack@Mou-ChundeMBP.MouLAN>
2017-10-10 00:19:06 +0800

    now trying to work with node_modules

--- functions/hello/index.js
+++ functions/hello/index.js
@@ -1,15 +1,32 @@
-console.log('starting function')
-exports.handle = function(event, ctx, callback) {
+//var request = require('request');
 
-  var input     = event;
-  var returnMsg = '';
+//console.log('starting function')
+exports.handle = function(event, context, callback) {
+
+  // var input     = event;
+  // var returnMsg = '';
+
+  //var bodyData = JSON.parse(event.body);
+  //var bodyData = '1234';
+  var bodyData = 'and your name is :' + event.name;
+  var areaCode = event.area;
+
+  var queryString = '';
+
+  var ApiPath = 'http://www.104.com.tw/i/apis/jobsearch.cfm';
 
 
+
+
+
+  //var returnJson = { "userinput" : bodyData };
   
-
-
-
-
-  console.log('processing event: %j', event)
-  callback(null, { hello: 'to this beautiful and cruel world' })
+  //console.log('processing event: %j', event)
+  //callback(null, { yolo: ', your name is' . name })
+  callback(null,
+          {
+            statusCode : 200,
+            headers : { 'Content-Type' : 'application/json' },
+            body : bodyData
+          });
 }
